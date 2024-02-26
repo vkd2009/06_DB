354ab912dd5199cb11c963c86ebb5ef9dde62292
inje <vkd2009@naver.com>
2024-02-26 16:49:54 +0900

    20240226_오늘 첫 DB 수업 내용

---
+++ 00_계정 생성(SYS 계정 접속).sql
@@ -0,0 +1,28 @@
+-- 한 줄 주석 (ctrl + /)
+
+/* 범위 주석  (ctrl + shifht + /) */
+
+/* SQL 1개 실행 : CTRL + ENTER
+ * 
+ * SQL 여러 개 실행 : (블럭 후) ALT + X
+ * */
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+
+-- 계정 생성
+CREATE USER KH_SIJ IDENTIFIED BY KH1234;
+
+-- 생성된 계정에
+-- 접속 + 기본 자원 관리 권한 추가
+GRANT CONNECT, RESOURCE TO KH_SIJ;
+
+-- 객체 생성 공간 할당
+ALTER USER KH_SIJ
+DEFAULT TABLESPACE SYSTEM 
+QUOTA UNLIMITED ON SYSTEM;
+
+
+
+
+
